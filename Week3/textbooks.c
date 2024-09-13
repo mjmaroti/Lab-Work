4dbf3ad11c21c36a560a51e282643283d3bc55a9
Isaiah Hilkemann <ihilkema@c28-0112-13.ad.mtu.edu>
2024-09-13 11:00:48 -0400

    add textbooks.c

---
+++ Week3/textbooks.c
@@ -0,0 +1,42 @@
+/*textbooks.c
+ * Isaiah Hilkemann
+ *Week 3 lab Q3
+ *9/13/24
+ *evaluates the amount of textbooks someone bought
+ */
+
+#include <stdio.h>
+
+int main(){
+	
+	int num = 0;
+	
+	printf("How many textbooks did you buy this semester?");
+
+	scanf("%d", &num);
+
+	switch(num){
+		case 0:
+			printf("you got away cheap!");
+		break;
+		case 1:
+		case 2:
+		case 3:
+			printf("that's a normal amount");	
+		break;
+		default:
+			if (num>=4)
+
+			{
+				printf("that's pretty expensive");
+			}
+			if (num<0)
+
+			{
+				printf("that's not an option!");
+			}
+
+	}
+
+
+}
