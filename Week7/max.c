91a0d195716bab23bf6519a673f3c28cc3a50156
Ryan020202 <rsmccabe@mtu.edu>
2024-10-11 11:27:16 -0400

    Create max.c

---
+++ Week7/max.c
@@ -0,0 +1,29 @@
+//Ryan McCabe
+//10/11/24
+//Code that finds the max when entering three integers 
+
+
+#include <stdio.h>
+
+int main() {
+    int Int1, Int2, Int3;
+    int maximum;
+
+    // Read three integers
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &Int1, &Int2, &Int3);
+
+    // Find the maximum
+    maximum = Int1; // Assume Int1 is the maximum
+    if (Int2 > maximum) {
+        maximum = Int2; // Update if Int2 is greater
+    }
+    if (Int3 > maximum) {
+        maximum = Int3; // Update if Int3 is greater
+    }
+
+    // Print the result
+    printf("The maximum is: %d\n", maximum);
+    
+    return 0;
+}
