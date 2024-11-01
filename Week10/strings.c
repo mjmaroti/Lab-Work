a6514950f41efa40bd38fe83517d52808edf54a3
Ryan020202 <rsmccabe@mtu.edu>
2024-11-01 16:11:49 -0400

    Update strings.c
    I fixed: memory allocation, typo, and added some new code.

--- Week10/strings.c
+++ Week10/strings.c
@@ -1,13 +1,13 @@
 // Names: Michael Marotich, Ryan McCabe, Isaiah Hilkemann
 // CS1111
-//ihilkema@mtu.edu, mjmaroti@mtu.edu, rsmccabe@mtu.edu
+// ihilkema@mtu.edu, mjmaroti@mtu.edu, rsmccabe@mtu.edu
 // 10/25/2025
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void bubbleSort(char **array, int size) {//bubble sort
+void bubbleSort(char **array, int size) {
     printf("Start: ");
     for (int i = 0; i < size; ++i) {
         printf("%s", array[i]);
@@ -18,27 +18,22 @@ void bubbleSort(char **array, int size) {//bubble sort
     for (int step = 0; step < size - 1; ++step) {
         int swapped = 0;
         for (int i = 0; i < size - step - 1; ++i) {
-            // compare 2 adjacent values
+            // Compare 2 adjacent values
             if (strncmp(array[i], array[i + 1], 20) > 0) {
-              
-                
-                // swap the strings
+                // Swap the strings
                 char *temp = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = temp;
-                swapped = 1; // mark if a swap occurs
-
-              
+                swapped = 1; // Mark if a swap occurs
             }
         }
-        
         if (swapped == 0) {
             break;
         }
     }
 }
 
-void printArray(char **array, int size) {//printing output
+void printArray(char **array, int size) {
     for (int i = 0; i < size; ++i) {
         printf("%s", array[i]);
         if (i < size - 1) printf(", ");
@@ -46,47 +41,44 @@ void printArray(char **array, int size) {//printing output
     printf("\n");
 }
 
-int main() {//main function
+int main() {
     const int size = 10;
     char **data = malloc(size * sizeof(char*));
-    
-    // data EVERYTHING AFTER THIS IS WHAT I EDITED. I HAVE NOT TRIED TO RUN IT
-    char *buffer = malloc(1024);
-    
-    const char **numbers[] = malloc(sizeof(char*) * size);
+    if (!data) {
+        fprintf(stderr, "Failed to allocate memory for data array!\n");
+        return -1;
+    }
 
-    for(int i = 0; i < nameCount; i++)
-    {
-        fgets(buffer, 1024, stdin);
-        buffer[strnlen(buffer, 1024) - 1] = 0;
-        numbers[i] = malloc(strnlen(buffer, 1024) + 1);
+    char buffer[1024];
 
-        if(!numbers[i])
-        {
-            printf("Failed to allocate!\n");
+    printf("Enter up to %d names:\n", size);
+    for (int i = 0; i < size; i++) {
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            break; // Stop if there is an error or EOF
+        }
+        buffer[strcspn(buffer, "\n")] = '\0'; // Remove the newline character
+
+        data[i] = malloc(strlen(buffer) + 1);
+        if (!data[i]) {
+            fprintf(stderr, "Failed to allocate memory for string!\n");
             return -1;
         }
+        strcpy(data[i], buffer); // Copy the string
+    }
 
-        strncpy(numbers[i], buffer, strnlen(buffer, 1024) + 1);
-    }
-    
-    // I think this for loop is what was confusing, it may not be necessary
-    /* for (int i = 0; i < size; ++i) {
-        data[i] = malloc(20 * sizeof(char)); //string space
-        strncpy(data[i],numbers[i], 20); //copy string
-        data[i][19] = '\0';//string temination
-    }
-    */
-    bubbleSort(data, size);
-    
+    // Actual number of entered names (in case fewer than size were entered)
+    int nameCount = size; // You might want to adjust this if fewer names are processed
+
+    bubbleSort(data, nameCount);
+
     printf("Alphabetical array:\n");
-    printArray(data, size);
-    
-    // free memory
-    for (int i = 0; i < size; ++i) {
-        free(data[i]); // free strings
+    printArray(data, nameCount);
+
+    // Free memory
+    for (int i = 0; i < nameCount; ++i) {
+        free(data[i]); // Free individual strings
     }
-    free(data); //free the array
+    free(data); // Free the array of pointers
 
     return 0;
 }
