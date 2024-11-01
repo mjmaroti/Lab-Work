7cc02a7df862b9ce97dfad1272c8b30773580279
mjmaroti <mjmaroti@mtu.edu>
2024-11-01 13:47:24 -0400

    Update strings.c

--- Week10/strings.c
+++ Week10/strings.c
@@ -50,15 +50,33 @@ int main() {//main function
     const int size = 10;
     char **data = malloc(size * sizeof(char*));
     
-    // data
-    const char *numbers[] =
+    // data EVERYTHING AFTER THIS IS WHAT I EDITED. I HAVE NOT TRIED TO RUN IT
+    char *buffer = malloc(1024);
+    
+    const char **numbers[] = malloc(sizeof(char*) * size);
 
-    for (int i = 0; i < size; ++i) {
+    for(int i = 0; i < nameCount; i++)
+    {
+        fgets(buffer, 1024, stdin);
+        buffer[strnlen(buffer, 1024) - 1] = 0;
+        numbers[i] = malloc(strnlen(buffer, 1024) + 1);
+
+        if(!numbers[i])
+        {
+            printf("Failed to allocate!\n");
+            return -1;
+        }
+
+        strncpy(numbers[i], buffer, strnlen(buffer, 1024) + 1);
+    }
+    
+    // I think this for loop is what was confusing, it may not be necessary
+    /* for (int i = 0; i < size; ++i) {
         data[i] = malloc(20 * sizeof(char)); //string space
         strncpy(data[i],numbers[i], 20); //copy string
         data[i][19] = '\0';//string temination
     }
-
+    */
     bubbleSort(data, size);
     
     printf("Alphabetical array:\n");
