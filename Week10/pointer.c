6e226b8f8c843a16d18726d4657c7e1a1a18c14a
Ryan020202 <rsmccabe@mtu.edu>
2024-11-01 11:29:25 -0400

    Update pointer.c

--- Week10/pointer.c
+++ Week10/pointer.c
@@ -9,18 +9,23 @@ int main()
 int num = 5;
 double rate =5.4;
 char letter = 'B';
-
+  
+//declare pointers for double, letter and rate
 int *numP;
 double *rateP;
 char *letterP;
+
+// numP points to num, rapeP to rate and letterP to letter
 numP= &num;
 rateP= &rate;
 letterP= &letter;
 
+//Increasing numP, rateP and letterP by one
 (*numP)++;
 (*rateP)++;
 (*letterP)++;
 
+//Printing content of each pointer 
 printf("numP: %d, *numP: %d\n",numP,*numP);
 printf("rateP: %p, *rateP: %.1f\n", (void*)rateP,&rateP);
 printf("letterP: %p, *letterP: %c\n", (void*)letterP,*letterP);
