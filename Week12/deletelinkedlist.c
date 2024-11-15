1a53e25ec9266bb9de1301030731fbb022c9c11e
Ryan020202 <rsmccabe@mtu.edu>
2024-11-15 15:56:16 -0500

    Update deletelinkedlist.c

--- Week12/deletelinkedlist.c
+++ Week12/deletelinkedlist.c
@@ -1,26 +1,25 @@
 /*
-* Implementation of deleteLinkedList
-*
-* Ryan McCabe
-* rsmccabe@mtu.edu
-*
-* @date 11/15/2024
-*/
+ * Implementation of deleteLinkedList
+ *
+ * @author Ryan McCabe
+ * @email rsmccabe@mtu.edu
+ *
+ * @date 11/15/2024
+ */
 #include "linkedlist.h"
 #include <stdlib.h>
 
 /*
-* Deletes the node at index from the linked list.
-*
-* @param head: memory address of pointer to first node in the list.
-* This will be updated to point at the new head if it changes
-* such as when deleting index 0.
-* @param index: The index to remove from the list.
-*
-* @return 0 on success, -1 on failure.
-*/
-int deleteLinkedList(Node **head, int index)
-{
+ * Deletes the node at index from the linked list.
+ *
+ * @param head: memory address of pointer to first node in the list.
+ * This will be updated to point at the new head if it changes
+ * such as when deleting index 0.
+ * @param index: The index to remove from the list.
+ *
+ * @return 0 on success, -1 on failure.
+ */
+int deleteLinkedList(Node **head, int index) {
     if (head == NULL || *head == NULL || index < 0) {
         // Invalid input or empty list
         return -1;
